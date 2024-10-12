181214e26b35c1d277c2c84072527b6a06455700
Harsh BHoir <harshbhoir1302@gmail.com>
2024-10-13 02:26:32 +0530

    made changes in routing sequence

--- src/app.js
+++ src/app.js
@@ -4,22 +4,22 @@ const express = require("express");
 // Create an instance of an Express application
 const app = express();
 
-// Set up a route for the root URL ("/")
-// When a request is made to "/", the server will respond with "hello from server"
-app.use("/", (req, res) => {
-  res.send("hello from server");
+// Set up a route for "/home"
+// When a request is made to "/home", the server will respond with "hello from server"
+app.use("/home", (req, res) => {
+  res.send("HOME");
 });
 
 // Set up a route for "/login"
 // There is a missing forward slash ("/") before "login". It should be "/login"
 app.use("/login", (req, res) => {
-  res.send("hello from server");
+  res.send("LOGIN");
 });
 
 // Set up a route for "/dashboard"
 // Similarly, there is a missing forward slash ("/") before "dashboard". It should be "/dashboard"
 app.use("/dashboard", (req, res) => {
-  res.send("hello from server");
+  res.send("DASHBOARD");
 });
 
 // Set up a route for the root URL ("/")
